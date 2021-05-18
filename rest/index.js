69d6915391b6b61a7a3bf01aad3682a252308bd8
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-05-18 15:47:35 -0400

    fixed items breaking after updates to innex.js and script.js further optimized for mobile use

--- rest/index.js
+++ rest/index.js
@@ -1,57 +1,72 @@
-// const express = require('express')
-// const app = express()
-// //const axios = require ('axios')**
-// const cors = require("cors");
-// const port = 7000;
-// app.use(cors());
-// const fetch = require("node-fetch");
-// const promises = [];
+const express = require('express')
+const app = express()
+//const axios = require ('axios')**
+const cors = require("cors");
+const port = 7000;
+app.use(cors());
+const fetch = require("node-fetch");
+const promises = [];
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// app.get("/pokemon/all", (req, res) => {
-//   for (let i = 1; i <= 150; i++) {
-//     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-//     //console.log("url error: "+ url);**
-//     promises.push(fetch(url).then((res) => 
-//       res.json()
-//     ));
-//   }
-//   Promise.all(promises).then((results) => {
-//     const pokemonResponse = results.map((result) => ({
-//       name: result.name,
-//       image: result.sprites["front_default"],
-//       type: result.types.map((type) => type.type.name).join(", "),
-//       id: result.id,
-//     }));
-//     res.send(pokemonResponse);
-//   });
-// });
+app.get("/pokemon/all", (req, res) => {
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    //console.log("url error: "+ url);**
+    promises.push(fetch(url).then((res) => 
+      res.json()
+    ));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemonResponse = results.map((result) => ({
+      name: result.name,
+      image: result.sprites["front_default"],
+      type: result.types.map((type) => type.type.name).join(", "),
+      id: result.id,
+    }));
+    res.send(pokemonResponse);
+  });
+});
+
+app.get("/pokemon/:i", (req, res) => {
+
+    console.log(req.params.i)
+    
+    const url = `https://pokeapi.co/api/v2/pokemon/${req.params.i}`;
+    fetch(url)
+    .then(res => res.json())
+    .then((result) => {
+        const pokemonResponse = {
+            name: result.name,
+            image: result.sprites["front_default"],
+            type: result.types.map((type) => type.type.name).join(", "),
+            id: result.id,
+        }
+        res.send(pokemonResponse);
+    });
+});
+
+app.get("/pokemon/random", (req, res) => {
+  for (let i = 1; i <= 151; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch("url").then((res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemonResponse = results.map((result) => ({
+      name: result.name,
+      image: result.sprites["front_default"],
+      type: result.types.map((type) => type.type.name).join(", "),
+      id: result.id,
+    }));
+    res.send(pokemonResponse);
+  });
+});
 
 
-// app.get("/pokemon/random", (req, res) => {
-//   for (let i = 1; i <= 150; i++) {
-//     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-//     promises.push(fetch("url").then((res) => res.json()));
-//   }
-//   Promise.all(promises).then((results) => {
-//     const pokemonResponse = results.map((result) => ({
-//       name: result.name,
-//       image: result.sprites["front_default"],
-//       type: result.types.map((type) => type.type.name).join(", "),
-//       id: result.id,
-//     }));
-//     res.send(pokemonResponse);
-//   });
-// });
+app.listen(port, () => {
+  console.log(`Poke app listening at http://localhost: ${port}`);
+});
 
 
-
-// app.listen(port, () => {
-//   console.log(`Poke app listening at http://localhost: ${port}`);
-// });
-
-
-// //<img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"**
--- web/script.js
+++ web/script.js
@@ -28,26 +28,88 @@ function playAudio() {
 }
 
 
-//Search
-// function mySearch() {
-//   var input, filter, div, h3, a, i, txtValue;
-//   input = document.getElementById("poke_container");
-//   filter = input.value.toUpperCase();
-//   div = document.getElementsByClassName("pokemon");
-//   h3 = ul.getElementsByClassName("name");
-//   for (i = 0; i < h3.length; i++) {
-//       a = h3[i].getElementsByClassName("name")[0];
-//       txtValue = name.textContent || name.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//           li[i].style.display = "";
-//       } else {
-//           li[i].style.display = "none";
-//       }
-//   }
-// }
+const poke_container =
+document.getElementById('poke_container');
+const pokemon_number = 151;
+const pokecardBig = "pokecardBig";
 
 
-//Big Pokemon Card on click function
+const colors = { //used (colorlib.com/etc/color-mixer.html) for combinations
+fire: '#fd7d25',
+grass: '#9bcc50',
+electric: '#eed534',
+water: '#4591c4',
+ground: '#CFB73F', //a combination of '#f7df3f' and '#ab9842'
+rock: '#a38c22',
+fairy: '#fdb8e9',
+poison: '#b97ec8',
+bug: '#72a03f',
+dragon: '#AA828C', //a combination of '#52a3ce' and '#f16e57'
+psychic: '#f366b9',
+flying: '#7CBED4', //a combination of '#3dc7ef' and '#bdb9b6'
+fighting: '#d56722',
+normal: '#a4acaf',
+ghost: '#7c63a3',
+ice: '#52c5e7',
+steel: '#9eb7b8',
+};
+  
+
+const main_types = Object.keys(colors);
+
+const fetchPokemon = async() => {
+  for(let i=1; i<=pokemon_number; i++){
+      await getPokemon(i);
+  }
+}
+
+const getPokemon = async id => {
+  const url = `http://localhost:7000/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  console.log(pokemon)
+  createPokemonCard(pokemon);
+}
+
+fetchPokemon();
+
+function createPokemonCard(pokemon) {
+  const pokemonEl = document.createElement('div');
+  
+  pokemonEl.classList.add('pokemon');
+  const types = pokemon.type.split(", ")
+  const typeA = types[0];
+  let typeB = "";
+  if (types.length > 1) {
+    typeB = types[1];
+    pokemonEl.style.background = "conic-gradient(" + colors[typeB] + "," + colors[typeA] + ")";
+  }else{
+    typeB = "";
+    pokemonEl.style.background = colors[typeA];
+  }
+  const name = pokemon.name.charAt(0).toUpperCase() +
+        pokemon.name.slice(1);
+  
+  
+  const pokeInnerHTML = `
+  <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+  <h3 class="name">${name}</h3>
+  <div class="img-container">
+    <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+  </div>
+  <div class="infoType">
+  <div class="typeA">Type: ${typeA}</div>
+  <div class="typeB">${typeB}</div>
+  </div>
+  `;
+  
+  pokemonEl.innerHTML = pokeInnerHTML;
+  
+  poke_container.appendChild(pokemonEl);
+
+}
+
+
 document.getElementById("poke_container").onclick = function () {
   var div = document.createElement('bigcard');
   var div2 = document.createElement('bigcardface');
@@ -74,187 +136,42 @@ document.getElementById("poke_container").onclick = function () {
 
 }
 
-
-
-//Big Pokemon Card remove on click function
-
-
-
-const poke_container =
-document.getElementById('poke_container');
-const pokemon_number = 151;
-// const colors = {
-// 	fire: '#FDDFDF',
-// 	grass: '#DEFDE0',
-// 	electric: '#FCF7DE',
-// 	water: '#DEF3FD',
-// 	ground: '#FRE7DA',
-// 	rock: '#D5D5D4',
-// 	fairy: '#FCEAFF',
-// 	poison: '#98D7A5',
-// 	bug: '#F8D7A3',
-// 	dragon: '#97B3E6',
-// 	psychic: '#EAEDA1',
-// 	flying: '#F5F5F5',
-// 	fighting: '#E6E0D4',
-// 	normal: '#F5F5F5',
-// };
-
-const colors = { //used (colorlib.com/etc/color-mixer.html) for combinations
-fire: '#fd7d25',
-grass: '#9bcc50',
-electric: '#eed534',
-water: '#4591c4',
-ground: '#CFB73F', //a combination of '#f7df3f' and '#ab9842'
-rock: '#a38c22',
-fairy: '#fdb8e9',
-poison: '#b97ec8',
-bug: '#72a03f',
-dragon: '#AA828C', //a combination of '#52a3ce' and '#f16e57'
-psychic: '#f366b9',
-flying: '#7CBED4', //a combination of '#3dc7ef' and '#bdb9b6'
-fighting: '#d56722',
-normal: '#a4acaf',
-ghost: '#7c63a3',
-ice: '#52c5e7',
-steel: '#9eb7b8',
-};
-
-// const colors = {
-//   fire: '#fd7d25',
-//   grass: '#9bcc50',
-//   electric: '#eed534',
-//   water: '#4591c4',
-//   ground: '#f7df3f','#ab9842', (#CFB73F)
-//   rock: '#a38c22',
-//   fairy: '#fdb8e9',
-//   poison: '#b97ec8',
-//   bug: '#72a03f',
-//   dragon: '#52a3ce','#f16e57', (#AA828C)
-//   psychic: '#f366b9',
-//   flying: '#3dc7ef','#bdb9b6', (#7CBED4)
-//   fighting: '#d56722',
-//   normal: '#a4acaf',
-//   };
-  
-const main_types = Object.keys(colors);
+//   //Big Pokemon Card on click function
+// document.getElementById("poke_container").onclick = function () {
+//   document.createElement('pokecardBig');
 
 
 
+  // pokecardBig.classList.add();
 
-const fetchPokemon = async() => {
-  for(let i=1; i<=pokemon_number; i++){
-      await getPokemon(i);
-  }
-}
+//   const typeA = pokemon.types[0].type.name;
+//   let typeB = "";
+//   if (pokemon.types.length > 1) {
+//     typeB = pokemon.types[1].type.name;
+//     pokecardBig.style.background = "conic-gradient(" + colors[typeB] + "," + colors[typeA] + ")";
+//   }else{
+//     typeB = "";
+//     pokecardBig.style.background = colors[typeA];
+//   }
+//   const name = pokemon.name[0].toUpperCase() +
+//         pokemon.name.slice(1);
 
-const getPokemon = async id => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  const res = await fetch(url);
-  const pokemon = await res.json();
-  createPokemonCard(pokemon);
-}
+//   const pokebigInnerHTML = `
+//   <span id= "id" class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+//   <h3 class="name">${name}</h3>
+//   <div class="img-container">
+//     <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+//   </div>
+//   <div class="infoType">
+//   <div class="typeA">Type: ${typeA}</div>
+//   <div class="typeB">${typeB}</div>
+//   </div>
+//   `;
 
-fetchPokemon();
+//   pokecardBig.innerHTML = pokebigInnerHTML;
 
-function createPokemonCard(pokemon) {
-  const pokemonEl = document.createElement('div');
-  
-  pokemonEl.classList.add('pokemon');
-  
-  const typeA = pokemon.types[0].type.name;
-  let typeB = "";
-  if (pokemon.types.length > 1) {
-    typeB = pokemon.types[1].type.name;
-    pokemonEl.style.background = "conic-gradient(" + colors[typeB] + "," + colors[typeA] + ")";
-  }else{
-    typeB = "";
-    pokemonEl.style.background = colors[typeA];
-  }
-  const name = pokemon.name[0].toUpperCase() +
-        pokemon.name.slice(1);
-  
-  
-  const pokeInnerHTML = `
-  <span id= "id" class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
-  <h3 class="name">${name}</h3>
-  <div class="img-container">
-    <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
-  </div>
-  <div class="infoType">
-  <div class="typeA">Type: ${typeA}</div>
-  <div class="typeB">${typeB}</div>
-  </div>
-  `;
-  
-  pokemonEl.innerHTML = pokeInnerHTML;
-  
-  poke_container.appendChild(pokemonEl);
-
-  
-}
-
-// let toSort = document.getElementById('id');
-
-// toSort = Array.prototype.slice.call(toSort, 0);
-let number = 
-console.log (number);
-
-// toSort.sort(function(a, b) {
-
-// });
-
-// let parent = document.getElementsByClassName('poke_container');
-// parent.innerHTML = "";
-
-// for(var i = 0, l = toSort.length; i < l; i++) {
-//     parent.appendChild(toSort[i]);
+//   poke_container.appendChild(pokecardBig);
 // }
-
-
-// function changeTheDisplay(pokemonEl) {
-//   content = document.querySelectorAll('.poke-container');
-//   content.forEach(row => pokemonEl.innerHTML = '');
-//   display(pokemonEl);
-// };
-
-// //SORT--- Pokemon by ID
-
-// //lowest to highest ID number
-// idUp = document.getElementById('idUp');
-// idUp.addEventListener('click', () => sortByAscendingId());
-
-// function sortByAscendingId() {
-//   pokemonEl.sort((a, b) => a.id - b.id);
-//   changeTheDisplay(pokemonEl);
-// };
-// //heighest to lowest ID number
-// idDown = document.getElementById('idDown');
-// idDown.addEventListener('click', () => sortByDescendingId());
-
-// function sortByDescendingId() {
-//   pokemonEl.sort((a, b) => b.id - a.id);
-//   changeTheDisplay(pokemonEl);
-// };
-
-//SORT--- Pokemon by Name
-//A - Z
-// nameUp = document.getElementById('nameUp');
-// nameUp.addEventListener('click', () => sortByAscendingName());
-
-// function sortByAscendingName() {
-//   pokemon.sort((a, b) => a.name.english < b.name.english ? -1 : 1);
-//   changeTheDisplay(pokemon);
-// };
-//Z - A
-// nameDown = document.getElementById('nameDown');
-// nameDown.addEventListener('click', () => sortByDescendingName());
-
-// function sortByDescendingName() {
-//   pokemon.sort((a, b) => a.name.english < b.name.english ? -1 : 1);
-//   pokemon.reverse();
-//   changeTheDisplay(pokemon);
-// };
 
 
 
@@ -318,33 +235,6 @@ console.log (number);
   
 // };
 
-// //This function retuns a color based on pokemon type
-// function colorPokemon(pokemon) {
-//   console.log("In colorPokemon: " + pokemon);
-//   console.log("Pokemon length" + pokemon.length);
-//   let pcolor = "";
-  
-//   //Set the card color based on the pokemon type
-//   if (pokemon === "electric") pcolor = "#cc9900";
-//   if (pokemon === "grass") pcolor = "#DEFDE0";
-//   if (pokemon === "poison") pcolor = "#DEFDE0";
-//   if (pokemon === "fire") pcolor = "#FDDFDF";
-//   if (pokemon === "water") pcolor = "#DEF3FD";
-//   if (pokemon === "flying") pcolor = "#F5F5F5";
-//   if (pokemon === "bug") pcolor = "#83901a";
-//   if (pokemon === "normal") pcolor = "#797964";
-//   if (pokemon === "ground") pcolor = "#bf9926";
-//   if (pokemon === "fairy") pcolor = "#bf9926";
-//   if (pokemon === "fighting") pcolor = "#bf9926";
-//   if (pokemon === "psychic") pcolor = "#bf9926";
-//   if (pokemon === "rock") pcolor = "#bf9926";
-//   if (pokemon === "steel") pcolor = "#bf9926";
-//   if (pokemon === "ice") pcolor = "#00aaff";
-//   if (pokemon === "ghost") pcolor = "#5454b3";
-//   if (pokemon === "dragon") pcolor = "#4e38e9";
-//   if (pokemon === "dark") pcolor = "#573e31";
 
-//   return pcolor;
-// }
 
 /* <img class="card-image" src="${pokeman.image}"/> */
